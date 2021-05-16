1fb7cebc4904b20ca50abff3a6ab8c283fb87a9a
Zielke Tevin <tz024@hdm-stuttgart.de>
2021-05-16 09:05:02 +0200

    Replaced Feed.js with stable version

--- src/js/Feed.js
+++ src/js/Feed.js
@@ -2,22 +2,12 @@ import React from 'react'
 import '../scss/App.scss';
 
 
-window.onload = function Feed() {
-  function viewContent() {
-      var el = document.getElementById(omo);
-          // el.style.opacity = 1;
-          if(el.style.opacity === "0" ) {
-              el.style.display = "1";
-          }
-          else {
-              el.style.display = "0";
-          }
-    
+function Feed() {
   return (
     <div className="feed">
       <section className="feedSection">
-        <p className="feedHeadlineItem" id="h01062021" onMouseOver={viewContent("c01062021")}>01.06.2021 - Jetzt neu im Shop: Sticker und Plakate</p>
-        <p className="feedHeadlineItem" id="h29052021" onMouseOver={viewContent("c29052021")}>29.05.2021 - AKTION 20.06.2021: Stuttgart fängt die Pandas ein </p>
+        <p className="feedHeadlineItem" id="h01062021">01.06.2021 - Jetzt neu im Shop: Sticker und Plakate</p>
+        <p className="feedHeadlineItem" id="h29052021">29.05.2021 - AKTION 20.06.2021: Stuttgart fängt die Pandas ein </p>
         <p className="feedHeadlineItem">27.05.2021 - Live-Debatte: "Strategien gegen die Panda Plage"</p>
         <p className="feedHeadlineItem">18.05.2021 - "Wie konnte das überhaupt passieren?" - Expertenrunde in Live-Konferenz</p>
         <p className="feedHeadlineItem">16.05.2021 - Mit Gummistiefeln durch die Königsstraße: Pandaexkremente unkontrollierbar</p>
@@ -33,8 +23,6 @@ window.onload = function Feed() {
       </section>
     </div>
   );
-  }
-
 }
 
 export default Feed;
