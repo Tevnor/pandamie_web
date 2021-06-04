784f5e1a8ad657a6b8b82b11811dbe4df85e3e59
tz024 <tz024@hdm-stuttgart.de>
2021-06-04 14:16:08 +0200

    updated shop order

--- src/js/Shop.js
+++ src/js/Shop.js
@@ -74,30 +74,6 @@ function Shop() {
             </button>
           </div>
         </div>
-      </div> 
-      <div className="hoodies">
-        <div className="shopItem" id="si4">
-          <div className="shopItemContent">
-            <h2 className="shopItemTitle">Munching Panda Hoodie</h2>
-            <p className="shopItemText">Donec mollis, erat id hendrerit imperdiet, turpis ipsum dapibus ligula, eget tristique odio sapien finibus metus.</p>
-            <button className="shopButton">Größe wählen</button>
-            {/* <button 
-              className="colorButton" 
-              id="cpb1"
-              >
-            </button>
-            <button 
-              className="colorButton" 
-              id="cpb2"
-              >
-            </button>
-            <button 
-              className="colorButton" 
-              id="cpb3"
-              >
-            </button> */}
-          </div>
-        </div>
       </div>
       <div className="totes">
         <div className="shopItem" id="si5">
@@ -167,6 +143,31 @@ function Shop() {
           </div>
         </div>
       </div>
+      <div className="hoodies">
+        <div className="shopItem" id="si4">
+          <div className="shopItemContent">
+            <h2 className="shopItemTitle">Munching Panda Hoodie</h2>
+            <p className="shopItemText">Donec mollis, erat id hendrerit imperdiet, turpis ipsum dapibus ligula, eget tristique odio sapien finibus metus.</p>
+            <button className="shopButton">Größe wählen</button>
+            {/* <button 
+              className="colorButton" 
+              id="cpb1"
+              >
+            </button>
+            <button 
+              className="colorButton" 
+              id="cpb2"
+              >
+            </button>
+            <button 
+              className="colorButton" 
+              id="cpb3"
+              >
+            </button> */}
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
